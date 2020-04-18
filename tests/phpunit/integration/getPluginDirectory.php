49c646901984613c5d5ea8769b60ee9df0c420ba
Robert Gadon <rgadon107@gmail.com>
2020-04-18 13:34:32 -0500

    Add `/tests/` directory to run unit & WP integratiion tests.

---
+++ tests/phpunit/integration/getPluginDirectory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Tests _get_plugin_directory().
+ *
+ * @package     spiralWebDB\ExtendGiveWP\Tests\PHP\Integration
+ * @since       1.0.0
+ * @link        https://github.com/rgadon107/starter-plugin
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDB\ExtendGiveWP\Tests\PHP\Integration;
+
+use function spiralWebDB\ExtendGiveWP\_get_plugin_directory;
+use function spiralWebDB\ExtendGiveWP\Tests\PHP\get_plugin_root_dir;
+
+/**
+ * Class Tests_GetPluginDirectory
+ *
+ * @package spiralWebDB\ExtendGiveWP\Tests\PHP\Integration
+ */
+class Tests_GetPluginDirectory extends Test_Case {
+
+	/**
+	 * Test _get_plugin_directory() should return the plugin's root directory.
+	 */
+	public function test__get_plugin_directory_should_run_plugin_directory() {
+		$this->assertStringEndsWith( 'starter-plugin', _get_plugin_directory() );
+		$this->assertSame( rtrim( get_plugin_root_dir(), DIRECTORY_SEPARATOR ), _get_plugin_directory() );
+	}
+}
---
+++ tests/phpunit/integration/getPluginUrl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Tests _get_plugin_url().
+ *
+ * @package     spiralWebDB\ExtendGiveWP\Tests\PHP\Integration
+ * @since       1.0.0
+ * @link        https://github.com/rgadon107/starter-plugin
+ * @license     GNU-2.0+
+ */
+
+namespace spiralWebDB\ExtendGiveWP\Tests\PHP\Integration;
+
+use function spiralWebDB\ExtendGiveWP\_get_plugin_url;
+
+/**
+ * Class Tests_GetPluginUrl
+ *
+ * @package spiralWebDb\StarterPlugin\Tests\PHP\Integration
+ */
+class Tests_GetPluginUrl extends Test_Case {
+
+	/**
+	 * Test _get_plugin_url() should return the plugin's URL.
+	 */
+	public function test__get_plugin_url_should_run_plugin_url() {
+		$this->assertStringEndsWith( 'plugins/starter-plugin', _get_plugin_url() );
+	}
+}
