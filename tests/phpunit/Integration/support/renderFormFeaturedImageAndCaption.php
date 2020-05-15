3458c666d350e646ca7a2f7a46e8070fdb42485c
Robert Gadon <rgadon107@gmail.com>
2020-05-15 17:10:16 -0500

    Integration test file updates
    - Add 'tearDown()' method to test class to delete option after each run of the test method. 
    - Correct the priority number and qualified name of the function under test in the first test method.
    - Add the post factory back into the test method. 
    - Add and get the option 'extend-give-wp'. 
    - Test assertions that the option was set and added to the database. Var $attachment_id now returns expected value. 
    - Create and update an attachment fixture for the test method. 
    - Notes included on unsuccessful attempts to update 'post_excerpt' of the attachment object. Needed to render 'post_except in the view. 
    - Commented out test assertions in test method to reduce the level of 'noise' returned when running test method. 
    - Deleted empty data set in dataProvider to simplify output of test method.

--- tests/phpunit/Integration/support/renderFormFeaturedImageAndCaption.php
+++ tests/phpunit/Integration/support/renderFormFeaturedImageAndCaption.php
@@ -28,10 +28,20 @@ use function spiralWebDb\ExtendGiveWP\render_form_featured_image_and_caption;
 class Tests_RenderFormFeaturedImageAndCaption extends TestCase {
 
 	/**
+	 * Clean up the test environment after each test.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		// Database cleanup.
+		delete_option( 'extend-give-wp' );
+	}
+
+	/**
 	 *  Test should check callback registered to action hook has expected priority.
 	 */
 	public function test_callback_registered_to_action_hook_has_expected_priority() {
-		$this->assertEquals( 5, has_action( 'give_pre_form', 'spiralWebDb\ExtendGiveWP\get_give_donation_form_id' ) );
+		$this->assertEquals( 10, has_action( 'give_pre_form', 'spiralWebDb\ExtendGiveWP\render_form_featured_image_and_caption' ) );
 	}
 
 	/**
@@ -39,10 +49,36 @@ class Tests_RenderFormFeaturedImageAndCaption extends TestCase {
 	 *
 	 * @dataProvider addTestData
 	 */
-	public function test_should_return_the_donation_form_id_when_given_post_id( $form_id, $args, $starts_with, $ends_with ) {
-		$form_id       = get_give_donation_form_id( $form_id );
-		$attachment_id = $this->factory()->attachment->create_object( $args );
-		$post_excerpt  = get_post_field( 'post_excerpt', $attachment_id );
+	public function test_should_return_the_donation_form_id_when_given_post_id( $option, $excerpt, $starts_with, $ends_with ) {
+		$form_id = $this->factory()->post->create();
+		$form_id = get_give_donation_form_id( $form_id );
+
+		// Add option to database so it can be called.
+		add_option( 'extend-give-wp', $option );
+		$options = get_option( 'extend-give-wp', [] );
+
+		// Test that the option was added and returns the expected value.
+		$this->assertTrue( isset( $options ) );
+		$this->assertTrue( isset( $option ) );
+
+		$attachment_id = isset( $options['featured-image-id'] ) ? (int) $options['featured-image-id'] : 0;
+
+		// Create and update an attachment fixture for the test method.
+		$args       = [
+			'ID'           => $attachment_id,
+			'post_excerpt' => $excerpt,
+			'post_parent'  => $form_id,
+			'post_type'    => 'attachment',
+		];
+		$attachment = $this->factory()->attachment->create_object( $args );
+		// Note: use of core function `wp_update_post` failed to update post_excerpt of attachment object.
+		// Note: use of core function `wp_insert_attachment` returned 0. Failed to update attachment object.
+		// Note: use of factory method for $attachment property returned 0. Failed to update attachment object.
+		// Question: How can the attachment (featured image) object be updated to include both the $attachment_id AND the 'post_excerpt'?
+
+		// Note: This returns falsey (empty string). How to access the 'post_excerpt' from the attachment object?
+		$post_excerpt = get_post_field( 'post_excerpt', $attachment_id );
+
 		wp_get_attachment_image( $attachment_id, 'large', $icon = false, $attr = [ 'class' => 'featured-image' ] );
 
 		ob_start();
@@ -57,27 +93,14 @@ class Tests_RenderFormFeaturedImageAndCaption extends TestCase {
 	 *  Data provider for integration test method.
 	 */
 	public function addTestData() {
-		$form_id = $this->factory()->post->create();
-
 		return [
-			'post data is empty'     => [
-				'form_id'          => 0,
-				'attachment_args'  => [
-					'post_excerpt' => '',
+			'featured image is not empty' => [
+				'option'           => [
+					'featured-image-id' => 144,
 				],
-				'view_starts_with' => '',
-				'view_ends_with'   => '',
-			],
-			'post data is not empty' => [
-				'form_id'          => $form_id,
-				'options'          => [
-					'featured_image_id' => 144,
-				],
-				'attachment_args'  => [
-					'post_excerpt' => 'Members of the Cornerstone Chorale & Brass during their 2018 tour.',
-				],
+				'post_excerpt'     => 'Members of the Cornerstone Chorale & Brass during their 2018 tour.',
 				'view_starts_with' => <<<FEATURED_IMAGE_VIEW_STARTS_WITH
-<figure id="donation-form-featured-image" class="donate-page-featured-image attachment-0" aria-describedby="donation-form-featured-image">
+<figure id="donation-form-featured-image" class="donate-page-featured-image attachment-144" aria-describedby="donation-form-featured-image">
 FEATURED_IMAGE_VIEW_STARTS_WITH
 				,
 				'view_ends_with'   => <<<FEATURED_IMAGE_VIEW_ENDS_WITH
