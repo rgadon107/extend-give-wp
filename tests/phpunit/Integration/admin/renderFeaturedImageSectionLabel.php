136e8009b7b95f7eca65ff75abff9ef6a1e7bafc
Robert Gadon <rgadon107@gmail.com>
2020-05-18 15:35:23 -0500

    Initial commits for unit & integration test files.

---
+++ tests/phpunit/Integration/admin/renderFeaturedImageSectionLabel.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *  Tests for render_featured_image_section_label()
+ *
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @package    spiralWebDb\ExtendGiveWP\Tests\Integration
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\ExtendGiveWP\Tests\Integration;
+
+use spiralWebDb\ExtendGiveWP\tests\phpunit\Integration\TestCase;
+use function spiralWebDb\ExtendGiveWP\Admin\render_featured_image_section_label;
+
+/**
+ * Class Test_RenderFeaturedImageSectionLabel
+ *
+ * @covers ::\spiralWebDb\ExtendGiveWP\render_featured_image_section_label
+ *
+ * @group   extend-give-wp
+ * @group   admin
+ *
+ * phpcs:disable Squiz.Commenting.FunctionComment.MissingParamTag
+ */
+class Test_RenderFeaturedImageSectionLabel extends TestCase {
+	
+}
---
+++ tests/phpunit/Unit/admin/renderFeaturedImageSectionLabel.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *  Tests for render_featured_image_section_label()
+ *
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @package    spiralWebDb\ExtendGiveWP\Tests\Unit
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\ExtendGiveWP\Tests\Unit;
+
+use Brain\Monkey\Functions;
+use spiralWebDb\ExtendGiveWP\tests\phpunit\Unit\TestCase;
+use function spiralWebDb\ExtendGiveWP\Admin\render_featured_image_section_label;
+
+/**
+ * Class Test_RenderFeaturedImageSectionLabel
+ *
+ * @covers ::\spiralWebDb\ExtendGiveWP\render_featured_image_section_label
+ *
+ * @group   extend-give-wp
+ * @group   admin
+ *
+ * phpcs:disable Squiz.Commenting.FunctionComment.MissingParamTag
+ */
+class Test_RenderFeaturedImageSectionLabel extends TestCase {
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		require_once EXTEND_GIVE_WP_ROOT_DIR . '/src/admin/option-settings-admin.php';
+	}
