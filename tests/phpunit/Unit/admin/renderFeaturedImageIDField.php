df1b24ff008f80f6d31caf447a142c35e297feaf
Robert Gadon <rgadon107@gmail.com>
2020-05-18 19:16:53 -0500

    Build out unit test file.
    - Note: test assertion fails with 2nd data set ( 'non-empty data set' ). Function under test returns falsey (empty string) from output buffer. 
    - The ternary statement evaluates to false, and $attachment_id is assigned 0. 
    - The view file does not render.

--- tests/phpunit/Unit/admin/renderFeaturedImageIDField.php
+++ tests/phpunit/Unit/admin/renderFeaturedImageIDField.php
@@ -37,4 +37,48 @@ class Test_RenderFeaturedImageIDField extends TestCase {
 
 		require_once EXTEND_GIVE_WP_ROOT_DIR . '/src/admin/option-settings-admin.php';
 	}
+	
+	/**
+	 * @dataProvider addTestData
+	 */
+	public function test_should_render_featured_image_id_field( $option, $expected_view ) {
+		Functions\expect( 'get_option' )
+			->once()
+			->with( 'extend-give-wp', [] )
+			->andReturn( $option['featured-image-id'] );
+		Functions\expect( 'spiralWebDb\ExtendGiveWP\_get_plugin_dir' )->andReturn( EXTEND_GIVE_WP_ROOT_DIR );
+
+		ob_start();
+		render_featured_image_id_field();
+		$actual_view = ob_get_clean();
+
+		$this->assertEquals( $expected_view, $actual_view );
+	}
+
+	public function addTestData() {
+		return [
+			'empty data set'     => [
+				'option_data'   => [
+					'featured-image-id' => 0
+				],
+				'expected_view' => '',
+			],
+			'non-empty data set' => [
+				'option_data'   => [
+					'featured-image-id' => 144,
+				],
+				'expected_view' => <<<FEATURED_IMAGE_ID_FIELD
+<label>
+	<input id="featured-image-id" class="normal-text" name="extend-give-wp[featured-image-id]" type="number" min="1" aria-describedby="featured-image-attachment-id" value="144">
+	<p id="featured-image-input-label" class="description">Enter the image ID for the donation form featured image in the field above.</p>
+	<p id="featured-image-input-label" class="description">Get the ID by opening the Media Library, and select the featured image.</p>
+	<p id="featured-image-input-label" class="description">View the permalink on the ‘Attachment Details’ page. The ID is the value of the ‘?item=‘ parameter in the permalink.</p>
+</label>
+
+FEATURED_IMAGE_ID_FIELD
+				,
+			]
+		];
+	}
 }
+
